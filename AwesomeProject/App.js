19f79e00fcbc3f1356b8fe9b0a61f4321d4fa4f9
Amin Lu <luaamin@gmail.com>
2023-05-08 17:45:29 +0800

    feat: try rn

---
+++ AwesomeProject/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <TextBox placeholder="Your course goal!" style={styles.textBox}></TextBox>
+        <Button title="ADD GOAL" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textbox: {
+    margin: 16, padding: 6, borderWidth: 1
+  }
+});
